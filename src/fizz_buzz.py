2d92c8a2fff9980df2c03b5fc69560857c5c49e6
misha <andrushchak.mykhailo@chnu.edu.ua>
2024-02-27 23:13:45 +0200

    task 2 done

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,3 +1,15 @@
 def fizz_buzz(param):
   #Type your code here
-  return
+  result = ""
+  if param %3==0:
+    result+="Fizz"
+  if param %5==0:
+    result+="Buzz"
+  if not result:
+    result=str(param)
+  return result
+
+print(fizz_buzz(3))
+print(fizz_buzz(5))
+print(fizz_buzz(30))
+print(fizz_buzz(991))
