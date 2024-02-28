9843832f3291d70df65371dfa71334da27f3f7cc
misha <andrushchak.mykhailo@chnu.edu.ua>
2024-02-29 00:15:25 +0200

    task 4 done

--- src/calculate_fuel.py
+++ src/calculate_fuel.py
@@ -1,3 +1,10 @@
 def calculate_fuel(distance):
   # Type your code
-  return
+  if distance <= 0:
+    return 100
+  else:
+    fuel_needed = distance * 10
+    if fuel_needed < 100:
+      return 100
+    else:
+      return fuel_needed
